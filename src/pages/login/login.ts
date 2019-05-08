6898bed177a46657fbc468d50f027efea741080f
JnBean <jonas_Bien@gmx.de>
2019-05-08 10:57:17 +0200

    heroku deployment

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, AlertController, Platform } from 'ionic-angular';
 
 import { Validators, FormBuilder, FormGroup, FormControl } from '@angular/forms';
 
@@ -32,8 +32,20 @@ export class LoginPage {
   constructor(
     private navCtrl: NavController,
     private authService: AuthService,
-    private formBuilder: FormBuilder
+    private formBuilder: FormBuilder,
+    private alertCtrl: AlertController,
+    private platform: Platform
   ) {}
+
+  showPlatform() {
+      let text = 'I run on: ' + this.platform.platforms();
+      let alert = this.alertCtrl.create({
+        title: 'My Home',
+        subTitle: text,
+        buttons: ['Ok']
+      });
+      alert.present();
+    }
 
   ionViewWillLoad(){
     this.validations_form = this.formBuilder.group({
